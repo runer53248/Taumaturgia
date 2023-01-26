c4c4407ce1b481b5b181125b30d33785f0f44c8a
Runer <runer53248@gmail.com>
2023-01-26 03:10:55 +0100

    Simplified specializations of Action structs in 'ObjectActions.hpp'.

--- ObjectActions.hpp
+++ ObjectActions.hpp
@@ -3,14 +3,6 @@
 #include "Object/Classes/Classes.hpp"
 #include "Object/ObjectModel.hpp"
 
-template<> struct Attack<Weapon> : Attack<Accept> { // all Weapons can attack
-	using Attack<Accept>::operator();
-};
-
-template<> struct Defend<Armor> : Defend<Accept> { // all Armors can defend
-	using Defend<Accept>::operator();
-};
-
-template<> struct Defend<Helmet> : Defend<Accept> { // all Helmets can defend
-	using Defend<Accept>::operator();
-};
+template<> struct Attack<Weapon> : Attack<Accept> {}; // all Weapons can attack
+template<> struct Defend<Armor> : Defend<Accept> {}; // all Armors can defend
+template<> struct Defend<Helmet> : Defend<Accept> {}; // all Helmets can defend
