dd406022792308e982b9b91e0d061f16f06a5556
Runer <runer53248@gmail.com>
2024-02-22 17:51:11 +0100

    Changed back traits::accessName by remove unnecesary get method.

--- Object/Concepts/Traits/NameTraits.hpp
+++ Object/Concepts/Traits/NameTraits.hpp
@@ -51,12 +51,6 @@ struct accessName {
         return CustomAccessName<std::remove_cv_t<T>>::get(el);
     }
 
-    template <CustomNameAccessable T>
-        requires(not UserTypeNameAccessable<T>)
-    static decltype(auto) get(const T& el) {
-        return CustomAccessName<std::remove_cv_t<T>>::get(el);
-    }
-
     template <UserTypeNameAccessable T>
     static decltype(auto) get(T& el) {
         return el.template getType<Name>();
