b789bb6e91fe1c30928056afab76c8261131f4b6
Runer <runer53248@gmail.com>
2024-03-15 22:37:51 +0100

    Renamed derived_from_template_base into derived_from_property.

--- Object/Properties/Helpers/is_derived_from_template_base.hpp
+++ Object/Properties/Helpers/is_derived_from_template_base.hpp
@@ -1,17 +1,18 @@
 #pragma once
 #include <type_traits>
+#include "taged_list.hpp"
 
-template <typename derived, template <typename> typename base>
-struct is_derived_from_template_base_impl {
+template <typename derived, template <typename> typename property>
+struct is_derived_from_property_impl {
     template <typename T>
-    static constexpr std::true_type test(const base<T>*);        // for properties (ie. impl::Wearing_<T>)
-    static constexpr std::true_type test(const base<derived>*);  // for conditional properties aliases (ie. Wearing<T>)
+    static constexpr std::true_type test(const property<T>*);        // for properties (ie. impl::Wearing_<T>)
+    static constexpr std::true_type test(const property<derived>*);  // for conditional properties aliases (ie. Wearing<T>)
     static constexpr std::false_type test(...);
     using type = decltype(test(std::declval<const derived*>()));
 };
 
-template <typename derived, template <typename> typename base>
-using is_derived_from_template_base = typename is_derived_from_template_base_impl<derived, base>::type;
+template <typename derived, template <typename> typename property>
+using is_derived_from_property = typename is_derived_from_property_impl<derived, property>::type;
 
-template <typename T, template <typename> typename base>
-concept derived_from_template_base = is_derived_from_template_base<T, base>::value;
+template <typename T, template <typename> typename property>
+concept derived_from_property = is_derived_from_property<T, property>::value;
