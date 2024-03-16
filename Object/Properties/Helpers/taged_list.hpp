3f862e1d6c083706233fc22bd9d0cb816e9facb6
Runer <runer53248@gmail.com>
2024-03-16 10:13:26 +0100

    Added is_inner_reduction and is_property_with_inner_reduction concepts into taged_list file. Constraints taged_list with them.

--- Object/Properties/Helpers/taged_list.hpp
+++ Object/Properties/Helpers/taged_list.hpp
@@ -1,15 +1,25 @@
 #pragma once
+#include <concepts>
 
 template <typename... T>
 struct list {};
 
 struct tag {};
 
+template <typename T>
+struct property_tag : T {};
+
+template <template <typename...> typename T>
+concept is_inner_reduction = std::same_as<T<T<property_tag<T<T<tag>>>>>, property_tag<T<tag>>>;
+
 template <template <typename...> typename T>
 concept is_property = requires {
     typename T<tag>::property_data;
 };
 
+template <template <typename...> typename T>
+concept is_property_with_inner_reduction = is_property<T> and is_inner_reduction<T>;
+
 template <template <typename...> typename... properties>
-    requires(is_property<properties> and ...)
+    requires(is_property_with_inner_reduction<properties> and ...)
 using taged_list = list<properties<tag>...>;
