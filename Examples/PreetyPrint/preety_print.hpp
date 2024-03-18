2b7a266bae264c46652a88493bcd5a5f4f0487b5
Runer <runer53248@gmail.com>
2024-03-18 22:33:01 +0100

    Remove duplication in preety print.

--- Examples/PreetyPrint/preety_print.hpp
+++ Examples/PreetyPrint/preety_print.hpp
@@ -11,26 +11,25 @@
     }
 } print_new_line;
 
+[[maybe_unused]] struct {
+    template <typename T>
+    auto operator()(std::reference_wrapper<T>&& value) const {
+        std::cout << '\t';
+        return std::optional{value};
+    }
+} print_tab;
+
+void print_ref_sign(auto&& value) {
+    if constexpr (not std::is_const_v<std::remove_reference_t<decltype(value)>>) {
+        std::cout << "[&]";
+    }
+}
+
 struct {
     auto operator()(WrappedType<Health> auto&& value) const {
         auto& hp = value.get();
-        if constexpr (not std::is_const_v<std::remove_reference_t<decltype(hp)>>) {
-            std::cout << "[&]";  // value is reference_wrapper to non const type - can be changed
-        }
-
-        std::cout << "(Health: " << hp.value();
-        if (hp.value() == hp.maxValue()) {
-            std::cout << " MAX";
-        }
-        std::cout << ")";
-        if (not hp.effects().empty()) {
-            std::cout << " {Effects: ";
-            for (const auto& effect : hp.effects()) {
-                std::cout << effect.effectType();
-                std::cout << effect.duration();
-            }
-            std::cout << "}";
-        }
+        print_ref_sign(hp);
+        std::cout << hp;
         return std::optional{value};
     }
 } print_hp;
@@ -38,11 +37,8 @@ struct {
 struct {
     auto operator()(WrappedType<CureHealth> auto&& value) const {
         auto& cureHealth = value.get();
-        if constexpr (not std::is_const_v<std::remove_reference_t<decltype(cureHealth)>>) {
-            std::cout << "[&]";
-        }
-
-        std::cout << "(Cure Health: " << cureHealth.value() << toString(cureHealth.valueType()) << ")";
+        print_ref_sign(cureHealth);
+        std::cout << cureHealth;
         return std::optional{value};
     }
 } print_cure_hp;
@@ -53,55 +49,26 @@ concept WrappedProtectionOrArmorClass = WrappedType<T, ArmorClass> or WrappedTyp
 struct {
     auto operator()(WrappedProtectionOrArmorClass auto&& value) const {
         auto& armor = value.get();  // can be either Protection or ArmorClass
-        if constexpr (not std::is_const_v<std::remove_reference_t<decltype(armor)>>) {
-            std::cout << "[&]";
-        }
-
-        std::cout << "(Ac: " << armor.armorClass() << " to " << armor.location() << ") ";
-
-        if (not armor.protectEffects().empty()) {
-            std::cout << "(protection";
-            for (const auto& effect : armor.protectEffects()) {
-                std::cout << effect;
-            }
-            std::cout << ") ";
-        }
-
+        print_ref_sign(armor);
+        std::cout << armor;
         return std::optional{value};
     }
 } print_protection;
 
 struct {
-    auto operator()(WrappedType<ProtectionContainer> auto&& value) const {
+    auto operator()(WrappedType<WearContainer> auto&& value) const {
         auto& armorWear = value.get();
-        if constexpr (not std::is_const_v<std::remove_reference_t<decltype(armorWear)>>) {
-            std::cout << "[&]";
-        }
-
-        std::cout << "(armor class: " << armorWear.value() << ") ";
-
-        if (not armorWear.protectEffects().empty()) {
-            std::cout << "(protection";
-            for (const auto& effect : armorWear.protectEffects()) {
-                std::cout << effect;
-            }
-            std::cout << ") ";
-        }
-
-        return std::optional{value};
+        print_ref_sign(armorWear);
+        std::cout << armorWear;
+        return std::optional{value}.and_then(print_new_line).and_then(print_tab);
     }
 } print_wear;
 
 struct {
     auto operator()(WrappedType<Damage> auto&& value) const {
         auto& damage = value.get();
-        if constexpr (not std::is_const_v<std::remove_reference_t<decltype(damage)>>) {
-            std::cout << "[&]";
-        }
-
-        std::cout << "(Damage: " << damage.value() << damage.type() << damage.effect().effectType() << damage.effect().duration();
-        std::cout << damage.effect().state().effectState();
-        std::cout << ")";
+        print_ref_sign(damage);
+        std::cout << damage;
         return std::optional{value};
     }
 } print_dmg;
@@ -109,15 +76,12 @@ struct {
 struct {
     auto operator()(WrappedType<EffectTypeContainer> auto&& value) const {
         auto& effects = value.get();
-        if constexpr (not std::is_const_v<std::remove_reference_t<decltype(effects)>>) {
-            std::cout << "[&]";
-        }
-
-        std::cout << "(Restore:";
-        for (const auto& effect : effects) {
-            std::cout << effect;
-        }
-        std::cout << ")";
+        print_ref_sign(effects);
+        print_in_round_braces(
+            std::cout, "Restore",
+            [&] {
+                std::cout << effects;
+            });
         return std::optional{value};
     }
 } print_restore;
