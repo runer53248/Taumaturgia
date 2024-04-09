cab8942902d2c840b2f3c3fdccf9e590f58db7c0
Runer <runer53248@gmail.com>
2024-04-09 06:52:11 +0200

    Added have_properties concept.

--- Examples/MainExamples/helpers_examples/main_have_properties.cpp
+++ Examples/MainExamples/helpers_examples/main_have_properties.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Examples/demangle_type_name.hpp"
+#include "Taumaturgia/Properties/Helpers/have_properties.hpp"
 #include "Taumaturgia/Properties/Properties.hpp"
 #include "Taumaturgia/Properties/UserProperty.hpp"
 
@@ -13,89 +14,60 @@ using IntProperty = UserProperty<int, T>;
 template <typename T>
 using FloatProperty = UserProperty<float, T>;
 
-template <template <typename...> typename property>
-    requires is_property<property>
-class equivalent_properties_predicate {
-private:
-    template <typename lhp, typename rhp>
-    struct equivalent_properties_predicate_impl {
-        static constexpr bool value =
-            // (std::is_same_v<lhp, rhp>) or                                                       // same property
-            (std::is_same_v<typename lhp::template type<tag>, typename rhp::template type<tag>>) or  // same Property or UserProperty (eg. UserProperty_<int, tag>)
-            ((lhp::value == rhp::value) and lhp::value != std::numeric_limits<size_t>::max());       // same priority of known property
-    };
-
-public:
-    template <typename T>
-    using type = equivalent_properties_predicate_impl<T, Property<property>>;
-};
-
-using helpers::Scheme;
-
-//TODO: add have_properties feature
-template <typename T, template <typename...> typename... properties>
-concept have_properties2 =
-    (is_property<properties> and ...) and
-    mp_and<
-        mp_to_bool<
-            mp_count_if<
-                typename Scheme<T>::list_t,
-                equivalent_properties_predicate<properties>::template type>>...>::value;
-
 using living_type = add_properties<Type, Living>;
 using int_property_type = add_properties<Type, IntProperty>;
 using float_property_type = add_properties<Type, FloatProperty>;
 using int_float_property_type = add_properties<Type, IntProperty, FloatProperty>;
 
-static_assert(have_properties2<living_type,
-                               Living>);
-static_assert(have_properties2<int_property_type,
-                               IntProperty>);
-static_assert(have_properties2<float_property_type,
-                               FloatProperty>);
-static_assert(have_properties2<int_float_property_type,
-                               FloatProperty>);
-static_assert(have_properties2<int_float_property_type,
-                               IntProperty,
-                               FloatProperty>);
+static_assert(have_properties<living_type,
+                              Living>);
+static_assert(have_properties<int_property_type,
+                              IntProperty>);
+static_assert(have_properties<float_property_type,
+                              FloatProperty>);
+static_assert(have_properties<int_float_property_type,
+                              FloatProperty>);
+static_assert(have_properties<int_float_property_type,
+                              IntProperty,
+                              FloatProperty>);
 
 using living_type = add_properties<Type, Living>;  // duplication of living_type
 
-static_assert(have_properties2<living_type,
-                               Living_impl>);
+static_assert(have_properties<living_type,
+                              Living_impl>);
 
 using living_healing_type = add_properties<Type, Living, Healing>;
 
-static_assert(have_properties2<living_healing_type,
-                               Living_impl,
-                               Healing_impl>);
-static_assert(have_properties2<living_healing_type,
-                               Healing_impl>);
-static_assert(have_properties2<living_healing_type,
-                               Healing_impl,
-                               Living>);
-static_assert(have_properties2<living_healing_type,
-                               Living,
-                               Healing_impl>);
-static_assert(have_properties2<living_healing_type,
-                               Living,
-                               Healing>);
-static_assert(have_properties2<living_healing_type,
-                               Healing>);
-static_assert(have_properties2<living_healing_type,
-                               Living>);
+static_assert(have_properties<living_healing_type,
+                              Living_impl,
+                              Healing_impl>);
+static_assert(have_properties<living_healing_type,
+                              Healing_impl>);
+static_assert(have_properties<living_healing_type,
+                              Healing_impl,
+                              Living>);
+static_assert(have_properties<living_healing_type,
+                              Living,
+                              Healing_impl>);
+static_assert(have_properties<living_healing_type,
+                              Living,
+                              Healing>);
+static_assert(have_properties<living_healing_type,
+                              Healing>);
+static_assert(have_properties<living_healing_type,
+                              Living>);
 
-static_assert(have_properties2<
+static_assert(have_properties<
               add_properties<Type, Living, Healing>,
               Living,
               Living,
               Healing,
               Living_impl,
-              Healing_impl>);  // TODO: consider removing duplicates and sorting properties before specialize have_properties2
-static_assert(have_properties2<
+              Healing_impl>);  // TODO: consider removing duplicates and sorting properties before specialize have_properties
+static_assert(have_properties<
               add_properties<Type, Living, Healing>,
               Healing_impl>);
-static_assert(not have_properties2<
+static_assert(not have_properties<
               add_properties<Type, Living>,
               Living_impl,
               Healing_impl>);
---
+++ Taumaturgia/Properties/Helpers/have_properties.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <limits>
+#include "property_helpers.hpp"
+
+namespace helpers {
+
+template <template <typename...> typename property>
+    requires is_property<property>
+class equivalent_properties_predicate {
+private:
+    template <typename lhp, typename rhp>
+    struct equivalent_properties_predicate_impl {
+        static constexpr bool value =
+            // (std::is_same_v<lhp, rhp>) or                                                       // same property
+            (std::is_same_v<typename lhp::template type<tag>, typename rhp::template type<tag>>) or  // same Property or UserProperty (eg. UserProperty_<int, tag>)
+            ((lhp::value == rhp::value) and lhp::value != std::numeric_limits<size_t>::max());       // same priority of known property
+    };
+
+public:
+    template <typename T>
+    using type = equivalent_properties_predicate_impl<T, Property<property>>;
+};
+
+}  // namespace helpers
+
+template <typename T, template <typename...> typename... properties>
+concept have_properties =
+    (is_property<properties> and ...) and
+    mp_and<
+        mp_to_bool<
+            mp_count_if<
+                typename helpers::Scheme<T>::list_t,
+                helpers::equivalent_properties_predicate<properties>::template type>>...>::value;
