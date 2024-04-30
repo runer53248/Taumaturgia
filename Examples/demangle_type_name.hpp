87e831d88010192ff67b5a754096ef47c6be7772
Runer <runer53248@gmail.com>
2024-04-30 18:45:26 +0200

    Improved name function in 'demangling_type_name.hpp' file.

--- Examples/demangle_type_name.hpp
+++ Examples/demangle_type_name.hpp
@@ -4,5 +4,18 @@
 
 template <typename T>
 inline std::string name() {
-    return boost::core::demangle(typeid(T).name());
+    using TR = std::remove_reference_t<T>;
+    std::string r = boost::core::demangle(typeid(T).name());
+    if (std::is_const<TR>::value) {
+        r = "const " + r;
+    }
+    if (std::is_volatile<TR>::value) {
+        r = "volatile " + r;
+    }
+    if (std::is_lvalue_reference<T>::value) {
+        r += "&";
+    } else if (std::is_rvalue_reference<T>::value) {
+        r += "&&";
+    }
+    return r;
 }
