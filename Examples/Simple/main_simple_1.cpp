6577c81b9f7e2b27d02e82ca1dd065fdc500eef3
Runer <runer53248@gmail.com>
2024-05-07 16:19:21 +0200

    Update main_simple_1 example.

--- Examples/Simple/main_simple_1.cpp
+++ Examples/Simple/main_simple_1.cpp
@@ -1,27 +1,14 @@
-#include <vector>
 #include "Examples/PreetyPrint/preety_print.hpp"
 #include "Examples/basic_strategies.hpp"
 #include "Taumaturgia/Properties/Properties.hpp"
 
 // MARK: Weapon_A
-
 struct Weapon_A {
     std::string name;
     Damage dmg;
 };
 
-#ifdef WITH_ADD_PROPERTIES
-template <typename T>
-    requires std::is_base_of_v<Weapon_A, T>
-struct traits::CustomAccessType<Damage, T> {
-    static constexpr decltype(auto) get(auto& el) {
-        return (el.dmg);
-    }
-};
-#endif
-
 // MARK: Weapon_B
-
 class Weapon_B {
 public:
     std::string name;
@@ -36,16 +23,7 @@ private:
     Damage dmg;
 };
 
-#ifdef WITH_ADD_PROPERTIES
-template <typename T>
-    requires std::is_base_of_v<Weapon_B, T>
-struct traits::CustomAccessType<Damage, T> {
-    static constexpr decltype(auto) get(auto& el) {
-        return el.getDamage();
-    }
-};
-#endif
-
+// MARK: Weapon_C
 class Weapon_C {
 public:
     std::string name;
@@ -60,111 +38,145 @@ private:
     Damage dmg;
 };
 
-// MARK: CustomAccessDamage<Weapon_C>
-
-#ifndef WITH_ADD_PROPERTIES
-template <typename T>
-    requires std::is_base_of_v<Weapon_C, T>
-struct traits::CustomAccessDamage<T> {
-#else
-template <typename T>
-    requires std::is_base_of_v<Weapon_C, T>
-struct traits::CustomAccessType<Damage, T> {
-#endif
-    static constexpr decltype(auto) get(auto& el) {
-        return el.Dmg();
-    }
-};
-
 // MARK: Weapon_D
-
-struct Default {};
 struct Empty {};
 using Weapon_D = add_properties<Empty, Naming, Damaging>;
 
 #ifdef WITH_ADD_PROPERTIES
-// override struct UserStrategy_<T, Default, ActionStatus> from basic_strategies
-template <>
-struct UserStrategy_<Damage, Default, ActionStatus> {
-    constexpr ActionStatus operator()(auto& obj, Object* owner, Object*) const {
-        std::cout << "X owner name: " << owner->name() << "   " << Damagingable_trait::get(obj) << '\n';
-        return ActionStatus::Success;
+// MARK: CustomAccessType<Damage, Weapon_A>
+template <typename T>
+    requires std::is_base_of_v<Weapon_A, T>
+struct traits::CustomAccessType<Damage, T> {
+    static constexpr decltype(auto) get(auto& el) {
+        return (el.dmg);
+    }
+};
+
+// MARK: CustomAccessType<Damage, Weapon_B>
+template <typename T>
+    requires std::is_base_of_v<Weapon_B, T>
+struct traits::CustomAccessType<Damage, T> {
+    static constexpr decltype(auto) get(auto& el) {
+        return el.getDamage();
     }
 };
 #endif
 
-// MARK: attack strategy
-
-template <Damagingable T>
-#ifndef NO_PREMADE_STRATEGIES
-struct AttackStrategy_<T> {
+#ifndef WITH_ADD_PROPERTIES
+// MARK: CustomAccessDamage<Weapon_C>
+template <typename T>
+    requires std::is_base_of_v<Weapon_C, T>
+struct traits::CustomAccessDamage<T> {
+    static constexpr decltype(auto) get(auto& el) {
+        return el.Dmg();
+    }
+};
 #else
-struct UserStrategy_<Damage, T, ActionStatus> {
-#endif
-    constexpr ActionStatus operator()([[maybe_unused]] Damagingable auto& obj, Object* owner, Object*) const {
-        std::cout << "owner name: " << owner->name() << "   " << Damagingable_trait::get(obj) << '\n';
-        return {};
+// MARK: CustomAccessType<Damage, Weapon_C>
+template <typename T>
+    requires std::is_base_of_v<Weapon_C, T>
+struct traits::CustomAccessType<Damage, T> {
+    static constexpr decltype(auto) get(auto& el) {
+        return el.Dmg();
     }
 };
+#endif
+
+// MARK: attack_impl_x
+inline constexpr ActionStatus attack_impl_x(Damagingable auto& obj, Object* owner, Object*) {
+    std::cout << "X owner name: " << owner->name() << "   " << Damagingable_trait::get(obj) << '\n';
+    return {};
+}
+
+#ifndef NO_PREMADE_STRATEGIES
+// MARK: AttackStrategy_<Weapon_D>
+// to override AttackStrategy_<Default>
+template <>
+struct AttackStrategy_<Weapon_D> {
+    constexpr ActionStatus operator()(Damagingable auto& obj, Object* owner, Object*) const {
+        return attack_impl_x(obj, owner, nullptr);
+    }
+};
+#else
+// MARK: UserStrategy_<Damage, Weapon_D, ...> 
+// override UserStrategy_<T, Default, ActionStatus>
+template <>
+struct UserStrategy_<Damage, Weapon_D, ActionStatus> {
+    constexpr ActionStatus operator()(Damagingable auto& obj, Object* owner, Object*) const {
+        return attack_impl_x(obj, owner, nullptr);
+    }
+};
+#endif
+
+// MARK: attack_impl
+inline constexpr ActionStatus attack_impl(Damagingable auto& obj, Object* owner, Object*) {
+    std::cout << "owner name: " << owner->name() << "   " << Damagingable_trait::get(obj) << '\n';
+    return {};
+}
+
+#ifndef NO_PREMADE_STRATEGIES
+// MARK: AttackStrategy_<Damagingable>
+template <Damagingable T>
+struct AttackStrategy_<T> {
+    constexpr ActionStatus operator()(Damagingable auto& obj, Object* owner, Object*) const {
+        return attack_impl(obj, owner, nullptr);
+    }
+};
+#else
+// MARK: UserStrategy_<Damage, Damagingable>
+template <Damagingable T>
+struct UserStrategy_<Damage, T, ActionStatus> {
+    constexpr ActionStatus operator()(Damagingable auto& obj, Object* owner, Object*) const {
+        return attack_impl(obj, owner, nullptr);
+    }
+};
+#endif
 
 // MARK: main
-
 int main() {
-    std::vector<Object> container;
-
     static_assert(Damagingable<Weapon_A>);
     static_assert(Damagingable<Weapon_B>);
     static_assert(Damagingable<Weapon_C>);
     static_assert(Damagingable<Weapon_D>);
 
+    auto default_damage = Damage{50,
+                                 Effect{
+                                     EffectType::Stun,
+                                     Duration{5, DurationType::Round},
+                                     EffectState::Active}};
+
     Object weapon_a = Weapon_A{
         Name{"Sword A"},
-        Damage{50,
-               Effect{
-                   EffectType::Stun,
-                   Duration{5, DurationType::Round},
-                   EffectState::Active}}};
+        default_damage};
 
     Object weapon_b = Weapon_B{
         Name{"Sword B"},
-        Damage{50,
-               Effect{
-                   EffectType::Stun,
-                   Duration{5, DurationType::Round},
-                   EffectState::Active}}};
+        default_damage};
 
     Object weapon_c = Weapon_C{
         Name{"Sword C"},
-        Damage{50,
-               Effect{
-                   EffectType::Stun,
-                   Duration{5, DurationType::Round},
-                   EffectState::Active}}};
+        default_damage};
 
     Object weapon_d = Weapon_D{
         Name{"Sword D"},
-        Damage{50,
-               Effect{
-                   EffectType::Stun,
-                   Duration{5, DurationType::Round},
-                   EffectState::Active}}};
+        default_damage};
 
     std::cout << "weapon attack:\n";
 
     attack(weapon_a, &weapon_a);
-    print_object_properties(weapon_a);
+    // print_object_properties(weapon_a);
     print_object(weapon_a);
 
     attack(weapon_b, &weapon_b);
-    print_object_properties(weapon_b);
+    // print_object_properties(weapon_b);
     print_object(weapon_b);
 
     attack(weapon_c, &weapon_c);
-    print_object_properties(weapon_c);
+    // print_object_properties(weapon_c);
     print_object(weapon_c);
 
     attack(weapon_d, &weapon_d);
-    print_object_properties(weapon_d);
+    // print_object_properties(weapon_d);
     print_object(weapon_d);
     std::cout << "\n";
 }
