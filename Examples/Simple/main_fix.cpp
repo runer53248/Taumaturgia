8beb0e27874726713a5bafa1640134283a75b493
Runer <runer53248@gmail.com>
2024-06-19 23:12:55 +0200

    Updated main_fix example.

--- Examples/Simple/main_fix.cpp
+++ Examples/Simple/main_fix.cpp
@@ -1,6 +1,5 @@
 #include <vector>
 #include "Examples/PreetyPrint/preety_print.hpp"
-
 #include "Usage/DefaultStrategies.hpp"
 #include "Usage/Properties.hpp"
 
@@ -36,25 +35,38 @@ static_assert(not is_custom_alive_strategy<Item>);
 
 struct Tile {
     std::string name;
-    Health hp;
     Damage dmg{3};
 };
-template <>
-#ifdef NO_PREMADE_STRATEGIES
-struct UserStrategy_<Health, Tile, std::optional<AliveStatus>> {
-#else
-struct AliveStrategy_<Tile> {
-#endif
-    constexpr std::optional<AliveStatus> operator()(Livingable auto&) const {
-        std::cout << "[D]";
-        return AliveStatus::Death;
-    }
+
+struct LivingTile : Tile {
+    Health hp{30};
 };
 
+template <typename T>
+    requires std::is_base_of_v<Tile, T>
+#ifdef NO_PREMADE_STRATEGIES
+struct UserStrategy_<Health, T, std::optional<AliveStatus>> {
+#else
+struct AliveStrategy_<T> {
+#endif
+    constexpr std::optional<AliveStatus> operator()(Livingable auto&) const {  // when have Livingable properties
+        std::cout << " [play death]";
+        return AliveStatus::Death;
+    }
+
+    constexpr std::optional<AliveStatus> operator()(auto&) const {  // when not have Livingable properties
+        std::cout << " [imitating living]";
+        return AliveStatus::Living;
+    }
+};
 
 static_assert(Gettingable<Tile>);
 static_assert(not is_custom_get_strategy<Tile>);
 static_assert(is_custom_alive_strategy<Tile>);
+
+static_assert(Gettingable<LivingTile>);
+static_assert(not is_custom_get_strategy<LivingTile>);
+static_assert(is_custom_alive_strategy<LivingTile>);
 
 // MARK: main
 
@@ -64,11 +76,16 @@ int main() {
         Health{30}};
 
     auto tile = Tile{
+        Name{"Tile"}};
+
+    auto living_tile = LivingTile{
         Name{"Grass"},
-        Health{30}};
+        Damage{0, Effect{EffectType::Burn}},
+        Health{60}};
 
     Object obj(item);
     Object obj_2(tile);
+    Object obj_3(living_tile);
 
     obj.getOpt<Properties::Health>();
     obj_2.getOpt<Properties::Health>();
@@ -81,4 +98,8 @@ int main() {
     print_object_properties(tile);
     print_customized_properties(tile);
     print_object(tile);
+
+    print_object_properties(living_tile);
+    print_customized_properties(living_tile);
+    print_object(living_tile);
 }
