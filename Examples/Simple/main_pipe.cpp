b8f2f92160fe5d0b8b10905a46eb9290c4e44f14
Runer <runer53248@gmail.com>
2024-07-25 08:37:01 +0200

    Update main_pipe example.

--- Examples/Simple/main_pipe.cpp
+++ Examples/Simple/main_pipe.cpp
@@ -1,4 +1,4 @@
-#define IGNORE_ORDER_LIST
+// #define IGNORE_ORDER_LIST
 
 // TODO: split preety prints requiring Object class from those that don't and group them for simple one include
 #include "Examples/PreetyPrint/PrintDamage.hpp"
@@ -8,6 +8,8 @@
 #include "Usage/Strategies.hpp"
 
 #include "Examples/demangle_type_name.hpp"
+
+#include <functional>
 
 namespace With {
 [[maybe_unused]] constexpr Property<Naming> Name{};
@@ -66,32 +68,72 @@ auto operator|(T, Property<Prop>) {
     };
 }
 
+template <template <typename...> typename Prop1, template <typename...> typename Prop2>
+auto operator|(Property<Prop1>, Property<Prop2>) {
+    return taged_list<Prop1, Prop2>{};
+}
+
+template <typename... Props, template <typename> typename Prop2>
+auto operator|(list<Props...>, Property<Prop2>) {
+    return boost::mp11::mp_append<list<Props...>, taged_list<Prop2>>{};
+}
+
+// for build in properties
+
+template <typename T, template <typename> typename... Props, typename P>
+auto fx(T, Props<P>...) {
+    using result = add_properties<T, Props...>;
+    return result{};
+}
+
+// for user type properties
+
+template <typename T, template <typename, typename> typename... Props, typename... TYPES, typename P>
+auto fx(T, Props<TYPES, P>...) {
+    using result = add_properties<T, UserPropertyAdapter<TYPES>::template type...>;
+    return result{};
+}
+
+//
+
+template <typename T, typename... Props>
+auto operator|(T, list<Props...>) {
+    return fx(T{}, Props{}...);
+}
+
 template <typename T, typename TYPE>
 concept type_of = std::same_as<std::remove_const_t<T>, TYPE>;
 
+#define TokenCtor(ClassName)                                         \
+    template <type_of<Token> Arg, typename... Args>                  \
+    ClassName(Arg&, Args&&... args) noexcept {                       \
+        ((trait<Args>::get(*this) = std::forward<Args>(args)), ...); \
+    }
+
 struct Base {
-    Name name;
-    Damage dmg;
-    Health hp;
-    Protection protection;
+    int x;
+    // Name name;
+    // Damage dmg;
+    // Health hp;
+    // Protection protection;
 
     Base() noexcept = default;
 
-    template <type_of<Token> Arg, typename... Args>
-    Base(Arg&, Args&&... args) noexcept {
-        ((trait<Args>::get(*this) = std::forward<Args>(args)), ...);
-    }
+    Base(int x)
+        : x{x} {}
 
-    // template <typename... Args>
-    // Base(Args&&... args) noexcept {
-    //     ((trait<Args>::get(*this) = std::forward<Args>(args)),...);
+    // template <type_of<Token> Arg, typename... Args>
+    // Base(Arg&, Args&&... args) noexcept {
+    //     ((trait<Args>::get(*this) = std::forward<Args>(args)), ...);
     // }
+
+    TokenCtor(Base)
 };
 
 int main() {
     std::cout << '\n';
-
     {
+        // create a type
         using type_1 = add_properties<
             Base,
             Living,
@@ -116,6 +158,34 @@ int main() {
     }
 
     {
+        // create a properties_list
+        using p_list = properties_list<
+            Living,
+            Damaging,
+            Protecting,
+            Naming>;
+
+        // create a type
+        using type_1 = p_list::apply_properties<Base>;
+
+        auto t1 = type_1{
+            token,  // ignore order of arguments
+            Damage{5, DamageType::Divine},
+            Health{100, 100},
+            Protection{10, BodyLocation::Head},
+            Name{"Test"},
+        };
+
+        std::cout << name<decltype(t1)>() << '\n';
+        std::cout << trait<Name>::get(t1) << '\n';
+        std::cout << trait<Damage>::get(t1) << '\n';
+        std::cout << trait<Protection>::get(t1) << '\n';
+        std::cout << trait<Health>::get(t1) << '\n';
+        std::cout << '\n';
+    }
+
+    {
+        // create a lambda factory
         auto create_type_2 =
             Base{}              //
             | With::Name        //
@@ -126,16 +196,51 @@ int main() {
 
         auto t2 = create_type_2(
             token,  // ignore order of arguments
+            Health{100, 100},
             Damage{5, DamageType::Magical},
             Protection{10, BodyLocation::Head},
-            Name{"Test"},
-            Health{100, 100});
+            Name{"Test"});
 
         std::cout << name<decltype(t2)>() << '\n';
         std::cout << trait<Name>::get(t2) << '\n';
         std::cout << trait<Damage>::get(t2) << '\n';
         std::cout << trait<Protection>::get(t2) << '\n';
         std::cout << trait<Health>::get(t2) << '\n';
+
+        // std::cout << traits::accessType<Name>::get(t2) << '\n';
+        // std::cout << traits::accessType<Damage>::get(t2) << '\n';
+        // std::cout << traits::accessType<Protection>::get(t2) << '\n';
+        // std::cout << traits::accessType<Health>::get(t2) << '\n';
+
         std::cout << '\n';
+    }
+
+    {
+        auto tlist = With::Name | With::Health | With::Protection | With::Damage;
+
+        Base base{12};
+
+        // modify existing object
+        auto tp = base | tlist;
+
+        std::cout << base.x << '\n';
+        std::cout << tp.x << '\n';  // ! should remember x value
+        std::cout << "tlist =  " << name<decltype(tlist)>() << '\n';
+        std::cout << "tp =     " << name<decltype(tp)>() << '\n';
+        std::cout << '\n';
+
+        [&]<typename... Args>(Args&&... args) {
+            ((trait<Args>::get(tp) = std::forward<Args>(args)), ...);
+        }(
+            Health{100, 100},
+            Damage{5},
+            Protection{10, BodyLocation::Head},
+            Name{"Test"});
+
+        std::cout << name<decltype(tp)>() << '\n';
+        std::cout << trait<Name>::get(tp) << '\n';
+        std::cout << trait<Damage>::get(tp) << '\n';
+        std::cout << trait<Protection>::get(tp) << '\n';
+        std::cout << trait<Health>::get(tp) << '\n';
     }
 }
