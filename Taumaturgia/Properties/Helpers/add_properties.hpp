060bd417b767d59b607e9abb2e2fcca40f341bf5
Runer <runer53248@gmail.com>
2024-11-27 00:43:23 +0100

    Renamed add_properties. Added new implementation of add_properties. Added property_type_ordered struct and add_properties_impl for both orderred and unordered properties.

--- Taumaturgia/Properties/Helpers/add_properties.hpp
+++ Taumaturgia/Properties/Helpers/add_properties.hpp
@@ -5,9 +5,9 @@
 namespace helpers::impl {
 template <typename T, template <typename...> typename... properties>
     requires(is_property<properties> and ...)
-struct add_properties_impl {
+struct add_properties_ordered_impl {
     using type = std::conditional_t<
-        have_property<T>,
+        have_property_data<T>,
         build_into_t<
             typename Scheme<T>::base,
             append_and_order_property_lists<
@@ -22,7 +22,7 @@ template <typename T, template <typename...> typename... properties>
     requires(is_property<properties> and ...)
 struct add_properties_unordered_impl {
     using type = std::conditional_t<
-        have_property<T>,
+        have_property_data<T>,
         build_into_t<
             typename Scheme_unordered<T>::base,
             append_and_order_property_lists<
@@ -33,12 +33,57 @@ struct add_properties_unordered_impl {
             create_unordered_property_list<properties...>>>;
 };
 
+template <typename property>
+struct property_type_ordered {};
+template <template <typename...> typename property>
+    requires is_property<property>
+struct property_type_ordered<Property<property>> {
+    static constexpr bool value = true;
+};
+template <template <typename...> typename property>
+    requires is_property<property>
+struct property_type_ordered<Property_unordered<property>> {
+    static constexpr bool value = false;
+};
+
+template <typename T, typename... properties>
+// requires(is_property<properties> and ...)
+struct add_properties_impl {
+    static constexpr auto num_of_ordered =  // count properties from Property
+        boost::mp11::mp_count<
+            list<boost::mp11::mp_bool<property_type_ordered<properties>::value>...>,
+            boost::mp11::mp_true>::value;
+    static_assert(num_of_ordered == 0 or num_of_ordered == sizeof...(properties));  // must be all or none
+
+    static constexpr bool is_ordered = num_of_ordered;
+
+    using scheme_type = std::conditional_t<
+        is_ordered,
+        Scheme<T>,
+        Scheme_unordered<T>>;
+
+    using type = std::conditional_t<
+        have_property_data<T>,
+        build_into_t<
+            typename scheme_type::base,
+            append_and_order_property_lists<
+                typename scheme_type::list_t,
+                create_property_list<properties...>>>,
+        build_into_t<
+            T,
+            create_property_list<properties...>>>;
+};
+
 }  // namespace helpers::impl
 
 template <typename T, template <typename...> typename... properties>
     requires(is_property<properties> and ...)
-using add_properties = helpers::impl::add_properties_impl<T, properties...>::type;
+using add_properties_ordered = helpers::impl::add_properties_ordered_impl<T, properties...>::type;
 
 template <typename T, template <typename...> typename... properties>
     requires(is_property<properties> and ...)
 using add_properties_unordered = helpers::impl::add_properties_unordered_impl<T, properties...>::type;
+
+template <typename T, typename... properties>
+// requires(is_property<properties> and ...)
+using add_properties = helpers::impl::add_properties_impl<T, properties...>::type;
