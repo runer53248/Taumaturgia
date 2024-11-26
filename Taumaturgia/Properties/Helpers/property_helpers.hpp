b85c37bb1f65cef0976b4020b2171b15bf378da1
Runer <runer53248@gmail.com>
2024-11-27 00:30:02 +0100

    Refactored properties_list aliases.

--- Taumaturgia/Properties/Helpers/property_helpers.hpp
+++ Taumaturgia/Properties/Helpers/property_helpers.hpp
@@ -1,9 +1,9 @@
 #pragma once
 #include "add_properties.hpp"
 
-//       taged_list        build_into_t  
+//       taged_list        build_into_t
 //            |                  |
-//      most_improved            |               
+//      most_improved            |
 //            |                  |
 //       same_priority           |
 //            |                  |
@@ -17,8 +17,14 @@ template <template <typename...> typename... properties>
     requires(is_property<properties> and ...)
 struct properties_list {
     template <typename T>
-    using apply_properties_to = ::add_properties<T, properties...>;
+    using apply_ordered_properties_to = ::add_properties_ordered<T, properties...>;
 
     template <typename T>
     using apply_unordered_properties_to = ::add_properties_unordered<T, properties...>;
+
+    template <typename T, bool ORDERED = true>
+    using apply_properties_to = std::conditional_t<
+        ORDERED,
+        ::add_properties_ordered<T, properties...>,
+        ::add_properties_unordered<T, properties...>>;
 };
