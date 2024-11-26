ab5c14d22b412b05efbebf1c48244e7371212426
Runer <runer53248@gmail.com>
2024-11-27 00:23:31 +0100

    Update Sheme struct.

--- Taumaturgia/Properties/Structs/Scheme.hpp
+++ Taumaturgia/Properties/Structs/Scheme.hpp
@@ -7,14 +7,14 @@ template <typename T>
 struct Scheme;
 
 template <typename T>
-    requires(not have_property<T>)
+    requires(not have_property_data<T>)
 struct Scheme<T> {
     using base = T;
     using list_t = list<>;
 };
 
 template <typename T>
-    requires(have_property<T>)
+    requires(have_property_data<T>)
 struct Scheme<T> {
     using base = Scheme<typename T::property_data::base_type>::base;  // most inner base type
     using list_helper = Scheme<typename T::property_data::base_type>::list_t;
@@ -27,14 +27,14 @@ template <typename T>
 struct Scheme_unordered;
 
 template <typename T>
-    requires(not have_property<T>)
+    requires(not have_property_data<T>)
 struct Scheme_unordered<T> {
     using base = T;
     using list_t = list<>;
 };
 
 template <typename T>
-    requires(have_property<T>)
+    requires(have_property_data<T>)
 struct Scheme_unordered<T> {
     using base = Scheme_unordered<typename T::property_data::base_type>::base;  // most inner base type
     using list_helper = Scheme_unordered<typename T::property_data::base_type>::list_t;
