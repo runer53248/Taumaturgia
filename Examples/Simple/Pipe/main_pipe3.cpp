70f742d46945a523271d900c46dc0e9fa04566f5
Runer <runer53248@gmail.com>
2024-12-04 08:31:14 +0100

    Updated main_pipe3 example. Moved update lambda to 'update.hpp' file.

--- Examples/Simple/Pipe/main_pipe3.cpp
+++ Examples/Simple/Pipe/main_pipe3.cpp
@@ -3,8 +3,8 @@
 #include "Base.hpp"
 #include "With.hpp"
 #include "default_values.hpp"
+#include "update.hpp"
 
-// TODO: split preety prints requiring Object class from those that don't and group them for simple one include
 #include "Examples/PreetyPrint/PrintDamage.hpp"
 #include "Examples/PreetyPrint/PrintHealth.hpp"
 #include "Examples/PreetyPrint/PrintName.hpp"
@@ -28,24 +28,10 @@ int main() {
         std::cout << "int    = " << trait<int>::get(type) << '\n';
         std::cout << "float  = " << trait<float>::get(type) << '\n';
         std::cout << "double = " << trait<double>::get(type) << '\n';
+        if constexpr (trait<std::string>::accessable<decltype(type)>) {
+            std::cout << "string = " << trait<std::string>::get(type) << '\n';
+        }
         std::cout << '\n';
-    };
-
-    // MARK: update lambda
-
-    // update rest of target parameters
-    auto update = [](auto& target) {
-        using boost::mp11::mp_unique;
-        using std::remove_cvref_t;
-        using std::same_as;
-
-        return [&target]<typename... Args>
-            requires(
-                (same_as<mp_unique<list<remove_cvref_t<Args>...>>, list<remove_cvref_t<Args>...>>) and         // every argument have unique type
-                (trait<remove_cvref_t<Args>>::template accessable<remove_cvref_t<decltype(target)>> and ...))  // every type is accessable by trait
-        (Args&&... args) {
-            ((trait<remove_cvref_t<Args>>::get(target) = std::forward<Args>(args)), ...);
-        };
     };
 
     // MARK: modify target
@@ -57,6 +43,9 @@ int main() {
     auto type4 = base | tlist;                                                          // modify existing target type
     auto type5 = base | (With::Name | With::Health | With::Protection | With::Damage);  // modify existing target type
 
+    static_assert(std::same_as<
+                  decltype(type3),
+                  decltype(type5)>);
     static_assert(std::same_as<
                   decltype(type4),
                   decltype(type5)>);
@@ -88,4 +77,106 @@ int main() {
     print(type3);
     print(type4);
     print(type5);
+
+    auto typex =
+        base                                //
+        | With::Name                        //
+        | With::Health                      //
+        | With::Protection                  //
+        | With::user_property<std::string>  //
+        | With::Damage                      //
+        | Create                            //
+        ;
+    auto typey =
+        Base{500, 100}                      //
+        | With::Name                        //
+        | With::Damage                      //
+        | With::user_property<std::string>  //
+        | With::Protection                  //
+        | With::Health                      //
+        | Create                            //
+        ;
+    typex.getType<std::string>() = "test x";
+    typey.getType<std::string>() = "test y";
+    print(typex);
+    print(typey);
+
+    struct Test {
+        int x;
+        int y;
+        Protection protection{};
+        CureHealth cureHealth{};
+
+        Test(int x, int y)
+            : x{x}, y{y} {
+            std::cout << "(int, int)\n";
+        }
+        Test(const Test&) {
+            std::cout << "(const&)\n";
+        }
+        Test(Test&&) {
+            std::cout << "op(&&)\n";
+        }
+        Test& operator=(const Test&) {
+            std::cout << "op=(const&)\n";
+            return *this;
+        }
+        Test& operator=(Test&&) {
+            std::cout << "op=(&&)\n";
+            return *this;
+        }
+    };
+
+    Test test{50, 10};
+    auto typez =
+        test                //
+        | With::Damage      //
+        | With::Health      //
+        | With::Health      //
+        | With::Protection  //
+        ;
+    std::cout << "type z      = " << name<decltype(typez)>() << '\n';
+    std::cout << "type z      = " << name<decltype(std::move(typez) | Create)>() << '\n';
+
+    auto typeq1 =
+        test                         //
+        | WithUnordered::Damage      //
+        | WithUnordered::Health      //
+        | WithUnordered::Health      //
+        | WithUnordered::Protection  //
+        ;
+    std::cout << "type q1     = " << name<decltype(typeq1)>() << '\n';
+    std::cout << "type q1     = " << name<decltype(std::move(typeq1) | Create)>() << '\n';
+    std::cout << "type q1     = " << name<decltype(typeq1 | Create)>() << '\n';
+    static_assert(std::is_same_v<
+                  decltype(std::move(typeq1) | Create),
+                  decltype(typeq1 | Create)>);
+    auto typeq2 =                    // * moved
+        Test{500, 100}               //
+        | WithUnordered::Protection  // skip
+        | WithUnordered::Damage      // * moved
+        | WithUnordered::Health      // * moved
+        | WithUnordered::CureHealth  // skip
+        | WithUnordered::Health      // skip
+        ;
+    std::cout << "type q2     = " << name<decltype(typeq2)>() << '\n';
+    std::cout << "type q2     = " << name<decltype(std::move(typeq2) | Create)>() << '\n';
+    std::cout << "type q2     = " << name<decltype(typeq2 | Create)>() << '\n';
+    static_assert(std::is_same_v<
+                  std::remove_reference_t<decltype(std::move(typeq2) | Create)>,
+                  std::remove_reference_t<decltype(typeq2 | Create)>>);
+
+    auto typew_list = With::Damage | With::Damage | With::Health | With::Health;
+
+    auto typew1 =       // moved once
+        Test{500, 100}  //
+        | typew_list;   // ! impl::pipe_helper depends on IGNORE_ORDER_LIST
+    std::cout << "type w1 list = " << name<decltype(typew_list)>() << '\n';
+    std::cout << "type w1      = " << name<decltype(typew1)>() << '\n';
+
+    auto typew2 =                                                                                           // moved once
+        Test{500, 100}                                                                                      //
+        | (WithUnordered::Damage | WithUnordered::Damage | WithUnordered::Health | WithUnordered::Health);  // ! impl::pipe_helper depends on IGNORE_ORDER_LIST
+    std::cout << "type w2 list = " << name<decltype((WithUnordered::Damage | WithUnordered::Damage | WithUnordered::Health | WithUnordered::Health))>() << '\n';
+    std::cout << "type w2      = " << name<decltype(typew2)>() << '\n';
 }
---
+++ Examples/Simple/Pipe/update.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <boost/mp11.hpp>
+#include <type_traits>
+#include "Taumaturgia/Properties/Helpers/taged_list.hpp"
+#include "Usage/Types/trait.hpp"
+
+// update rest of target parameters
+auto update = [](auto& target) {
+    using boost::mp11::mp_unique;
+    using std::remove_cvref_t;
+    using std::same_as;
+
+    return [&target]<typename... Args>
+        requires(
+            (same_as<mp_unique<list<remove_cvref_t<Args>...>>, list<remove_cvref_t<Args>...>>) and         // every argument have unique type
+            (trait<remove_cvref_t<Args>>::template accessable<remove_cvref_t<decltype(target)>> and ...))  // every type is accessable by trait
+    (Args&&... args) {
+        ((trait<remove_cvref_t<Args>>::get(target) = std::forward<Args>(args)), ...);
+    };
+};
