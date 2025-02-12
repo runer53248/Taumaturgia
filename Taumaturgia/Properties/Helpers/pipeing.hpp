c7c73820b2bde97fd908bc591d951205e69f8306
Runer <runer53248@gmail.com>
2025-02-12 18:34:26 +0100

    Reordered 'pipeing.hpp' implementation.

--- Taumaturgia/Properties/Helpers/pipeing.hpp
+++ Taumaturgia/Properties/Helpers/pipeing.hpp
@@ -15,8 +15,88 @@ using creator_add_properties = std::conditional_t<
     add_properties_unordered<T, properties...>,
     add_properties_ordered<T, properties...>>;
 
+//************************************************************************ */
+namespace impl {
+
+template <typename T, typename L>
+struct DataAndPropertiesList {
+    constexpr DataAndPropertiesList(T&& data) noexcept
+        : data{std::forward<T>(data)} {}
+
+    template <typename LL>
+    constexpr DataAndPropertiesList(DataAndPropertiesList<T, LL>&& other) noexcept
+        : data{std::forward<T>(other.data)} {}
+
+    T data;
+};
+
+}  // namespace impl
+
 struct {
 } Create;
+
+// Add new property to DataAndPropertiesList
+template <typename T, typename... Props, typename Prop2>
+decltype(auto) operator|(impl::DataAndPropertiesList<T, list<Props...>>&& tp, Prop2) {
+    using base_type = std::remove_cvref_t<T>;
+    using helper = Prop2::template apply<base_type>;
+    if constexpr (std::is_same_v<helper, base_type>) {  // ignore unnecesary property for type T
+        return (tp);
+    } else {
+        using namespace boost::mp11;
+        using prop_list = helpers::append_and_order_property_lists<list<Props...>, list<Prop2>>;
+        if constexpr (mp_contains<list<Props...>, Prop2>::value) {  // skip if property list contain Prop2
+            return (tp);
+        } else if constexpr (std::same_as<prop_list, list<Props...>>) {  // skip if property list don't change
+            return (tp);
+        } else {
+            using prop_list = helpers::append_and_order_property_lists<list<Props...>, list<Prop2>>;
+            return (impl::DataAndPropertiesList<T, prop_list>{std::forward<T>(tp.data)});
+        }
+    }
+}
+
+// Create new type object from DataAndPropertiesList
+template <typename T, typename... Props>
+decltype(auto) operator|(impl::DataAndPropertiesList<T, list<Props...>>&& tp, decltype(Create)) {
+    using namespace boost::mp11;
+    static_assert(
+        mp_count<list<mp_bool<helpers::impl::property_type_ordered<Props>::value>...>, mp_true>::value == sizeof...(Props)  //
+        or
+        mp_count<list<mp_bool<helpers::impl::property_type_ordered<Props>::value>...>, mp_false>::value == sizeof...(Props));
+    return (add_properties<std::remove_cvref_t<T>, Props...>{std::forward<T>(tp.data)});
+}
+// Create new type object from DataAndPropertiesList
+template <typename T, typename... Props>
+decltype(auto) operator|(impl::DataAndPropertiesList<T, list<Props...>>& tp, decltype(Create)) {
+    using namespace boost::mp11;
+    static_assert(
+        mp_count<list<mp_bool<helpers::impl::property_type_ordered<Props>::value>...>, mp_true>::value == sizeof...(Props) or
+        mp_count<list<mp_bool<helpers::impl::property_type_ordered<Props>::value>...>, mp_false>::value == sizeof...(Props));
+    return (add_properties<std::remove_cvref_t<T>, Props...>{std::forward<T>(tp.data)});
+}
+// usage of 'Create' pipe when T isn't DataAndPropertiesList type
+template <typename T, typename...>
+decltype(auto) operator|(T&& tp, decltype(Create)) {
+    return (tp);
+}
+
+// Start DataAndPropertiesList pipe if property is applyable
+template <typename T, typename Prop>
+    requires requires {
+        typename Prop::template apply<std::remove_cvref_t<T>>;
+    }
+decltype(auto) operator|(T&& t, Prop) {
+    using base_type = std::remove_cvref_t<T>;
+    using helper = Prop::template apply<base_type>;
+    if constexpr (std::is_same_v<helper, base_type>) {  // ignore unnecessary property for type T
+        return (t);
+    } else {
+        return (impl::DataAndPropertiesList<T, list<Prop>>{std::forward<T>(t)});
+    }
+}
+
+//************************************************************************ */
 
 namespace impl {
 
@@ -48,41 +128,19 @@ struct Creator {
     }
 };
 
-template <typename T, typename L>
-struct DataAndPropertiesList {
-    constexpr DataAndPropertiesList(T&& data) noexcept
-        : data{std::forward<T>(data)} {}
-
-    template <typename LL>
-    constexpr DataAndPropertiesList(DataAndPropertiesList<T, LL>&& other) noexcept
-        : data{std::forward<T>(other.data)} {}
-
-    T data;
-};
-
-// for build in properties
-template <typename T, template <typename> typename... Props, typename P>
-auto pipe_helper(T&& t, Props<P>...) {
-    // using result = add_properties<std::remove_cvref_t<T>, Property<Props<tag>::template apply>...>;
-    using result = ::creator_add_properties<std::remove_cvref_t<T>, Props<P>::template apply...>;
-    return result{std::forward<T>(t)};
-}
-
-// for user type properties
-template <typename T, template <typename, typename> typename... Props, typename... TYPES, typename P>
-auto pipe_helper(T&& t, Props<TYPES, P>...) {
-    using result = ::creator_add_properties<std::remove_cvref_t<T>, UserPropertyAdapter<TYPES>::template type...>;
-    return result{std::forward<T>(t)};
-}
-
 }  // namespace impl
 
 template <typename T>
 struct BaseType {};
 
 namespace From {
+
+// template <typename T>
+// struct BaseType {};
+
 template <typename T>
 [[maybe_unused]] constexpr BaseType<T> base{};  // can be used even when T don't have default C-tor.
+
 }  // namespace From
 
 template <
@@ -102,6 +160,27 @@ template <
 auto operator|(impl::Creator<T>, P<Prop>) -> impl::Creator<::add_properties<T, P<Prop>>> {
     return {};
 }
+
+//************************************************************************ */
+
+namespace impl {
+
+// for build in properties
+template <typename T, template <typename> typename... Props, typename P>
+auto pipe_helper(T&& t, Props<P>...) {
+    // using result = add_properties<std::remove_cvref_t<T>, Property<Props<tag>::template apply>...>;
+    using result = ::creator_add_properties<std::remove_cvref_t<T>, Props<P>::template apply...>;
+    return result{std::forward<T>(t)};
+}
+
+// for user type properties
+template <typename T, template <typename, typename> typename... Props, typename... TYPES, typename P>
+auto pipe_helper(T&& t, Props<TYPES, P>...) {
+    using result = ::creator_add_properties<std::remove_cvref_t<T>, UserPropertyAdapter<TYPES>::template type...>;
+    return result{std::forward<T>(t)};
+}
+
+}  // namespace impl
 
 template <
     template <template <typename...> typename> typename P1,
@@ -126,64 +205,6 @@ auto operator|(list<Props...>, P<Prop2>) {
         return list<Props...>{};
     } else {
         return mp_append<list<Props...>, taged_list<Prop2>>{};
-    }
-}
-
-// Create new type object from DataAndPropertiesList
-template <typename T, typename... Props>
-decltype(auto) operator|(impl::DataAndPropertiesList<T, list<Props...>>&& tp, decltype(Create)) {
-    using namespace boost::mp11;
-    static_assert(
-        mp_count<list<mp_bool<helpers::impl::property_type_ordered<Props>::value>...>, mp_true>::value == sizeof...(Props)  //
-        or
-        mp_count<list<mp_bool<helpers::impl::property_type_ordered<Props>::value>...>, mp_false>::value == sizeof...(Props));
-    return (add_properties<std::remove_cvref_t<T>, Props...>{std::forward<T>(tp.data)});
-}
-template <typename T, typename... Props>
-decltype(auto) operator|(impl::DataAndPropertiesList<T, list<Props...>>& tp, decltype(Create)) {
-    using namespace boost::mp11;
-    static_assert(
-        mp_count<list<mp_bool<helpers::impl::property_type_ordered<Props>::value>...>, mp_true>::value == sizeof...(Props) or
-        mp_count<list<mp_bool<helpers::impl::property_type_ordered<Props>::value>...>, mp_false>::value == sizeof...(Props));
-    return (add_properties<std::remove_cvref_t<T>, Props...>{std::forward<T>(tp.data)});
-}
-// pipe when T isn't DataAndPropertiesList type
-template <typename T, typename...>
-decltype(auto) operator|(T&& tp, decltype(Create)) {
-    return (tp);
-}
-
-template <typename T, typename... Props, typename Prop2>
-decltype(auto) operator|(impl::DataAndPropertiesList<T, list<Props...>>&& tp, Prop2) {
-    using base_type = std::remove_cvref_t<T>;
-    using helper = Prop2::template apply<base_type>;
-    if constexpr (std::is_same_v<helper, base_type>) {  // ignore unnecesary property for type T
-        return (tp);
-    } else {
-        using namespace boost::mp11;
-        using prop_list = helpers::append_and_order_property_lists<list<Props...>, list<Prop2>>;
-        if constexpr (mp_contains<list<Props...>, Prop2>::value) {  // skip if property list contain Prop2
-            return (tp);
-        } else if constexpr (std::same_as<prop_list, list<Props...>>) {  // skip if property list don't change
-            return (tp);
-        } else {
-            using prop_list = helpers::append_and_order_property_lists<list<Props...>, list<Prop2>>;
-            return (impl::DataAndPropertiesList<T, prop_list>{std::forward<T>(tp.data)});
-        }
-    }
-}
-
-template <typename T, typename Prop>
-    requires requires {
-        typename Prop::template apply<std::remove_cvref_t<T>>;
-    }
-decltype(auto) operator|(T&& t, Prop) {
-    using base_type = std::remove_cvref_t<T>;
-    using helper = Prop::template apply<base_type>;
-    if constexpr (std::is_same_v<helper, base_type>) {  // ignore unnecessary property for type T
-        return (t);
-    } else {
-        return (impl::DataAndPropertiesList<T, list<Prop>>{std::forward<T>(t)});
     }
 }
 
