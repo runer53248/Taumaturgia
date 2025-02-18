b62642a33f8d8868cad6d2f8c73730d2d0ac564f
Runer <runer53248@gmail.com>
2025-02-18 18:23:18 +0100

    Removed unused creator_add_properties from pipeing file.

--- Taumaturgia/Properties/Helpers/pipeing.hpp
+++ Taumaturgia/Properties/Helpers/pipeing.hpp
@@ -3,21 +3,6 @@
 #include "Usage/Properties.hpp"
 #include "taged_list.hpp"
 
-#ifdef IGNORE_ORDER_LIST
-constexpr bool ignore_order_list = true;
-#else
-constexpr bool ignore_order_list = false;
-#endif
-
-template <typename T, template <typename...> typename... properties>
-using creator_add_properties = std::conditional_t<
-    ignore_order_list,
-    add_properties_unordered<T, properties...>,
-    add_properties_ordered<T, properties...>>;
-
-//************************************************************************ */
 #include "pipeing_with_create.hpp"
-//************************************************************************ */
 #include "pipeing_with_creator.hpp"
-//************************************************************************ */
 #include "pipeing_with_property_list.hpp"
