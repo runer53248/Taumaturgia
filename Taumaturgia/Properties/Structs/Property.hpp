84e8b66ff308b2bc667d401752cde29de530436a
Runer <runer53248@gmail.com>
2025-07-11 16:31:43 +0200

    Added operator== method into Property and Property_unordered structs.

--- Taumaturgia/Properties/Structs/Property.hpp
+++ Taumaturgia/Properties/Structs/Property.hpp
@@ -27,6 +27,27 @@ public:
     template <template <typename...> typename P>
         requires is_property<P>
     using property_type = Property<P>;  // use same Property or Property_unordered
+
+    template <template <typename...> typename property_other>
+    constexpr bool operator==([[maybe_unused]] const Property<property_other>&) const {
+        using other_type = Property<property_other>;
+
+        if constexpr (
+            std::same_as<hold_type, typename other_type::hold_type>              // same hold_type
+            and std::same_as<property_data, typename other_type::property_data>  // same property_data
+            and std::same_as<tags_list, typename other_type::tags_list>          // same tags_list
+            and not(std::same_as<
+                        apply<apply<tag>>,
+                        apply<tag>> xor
+                    std::same_as<
+                        typename other_type::template apply<typename other_type::template apply<tag>>,
+                        typename other_type::template apply<tag>>)  // both apply once or both aren't
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 };
 
 template <template <typename...> typename property>
@@ -38,11 +59,33 @@ public:
     using apply = property<T>;
 
     using hold_type = property<tag>::hold_type;
+    using property_data = property<tag>::property_data;
     using tags_list = property<tag>::property_data::tags_list;
 
     template <template <typename...> typename P>
         requires is_property<P>
     using property_type = Property_unordered<P>;  // use same Property or Property_unordered
+
+    template <template <typename...> typename property_other>
+    constexpr bool operator==([[maybe_unused]] const Property_unordered<property_other>&) const {
+        using other_type = Property_unordered<property_other>;
+
+        if constexpr (
+            std::same_as<hold_type, typename other_type::hold_type>              // same hold_type
+            and std::same_as<property_data, typename other_type::property_data>  // same property_data
+            and std::same_as<tags_list, typename other_type::tags_list>          // same tags_list
+            and not(std::same_as<
+                        apply<apply<tag>>,
+                        apply<tag>> xor
+                    std::same_as<
+                        typename other_type::template apply<typename other_type::template apply<tag>>,
+                        typename other_type::template apply<tag>>)  // both apply once or both aren't
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 };
 
 template <template <template <typename...> typename> typename PROP_TYPE>
