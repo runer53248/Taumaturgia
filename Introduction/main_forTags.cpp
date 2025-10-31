9ed563ffc58f43da7f0d7640be8513842103fe5b
Runer <runer53248@gmail.com>
2025-10-31 16:06:34 +0100

    Updated forTags example.

--- Introduction/main_forTags.cpp
+++ Introduction/main_forTags.cpp
@@ -1,5 +1,6 @@
 #include <boost/pfr.hpp>
 #include <print>
+#include <utility>
 #include "Examples/PreetyPrint/preety_print_types.hpp"
 #include "Introduction/parse_type_name.hpp"
 #include "Usage/With.hpp"
@@ -11,9 +12,15 @@ struct Base {
     Data<int, extra_token> type;
 
     template <size_t DIG>
+        requires(DIG < 2)
     decltype(auto) getType(this auto& self) {
         return boost::pfr::get<DIG>(self);
     }
+};
+
+template <>
+struct UserDefaultValue<Damage, extra_token> {
+    static constexpr auto value = [] { return Damage{500}; };
 };
 
 template <typename T, typename Tags>
@@ -22,18 +29,40 @@ auto& operator<<(std::ostream& out, Data<T, Tags> status) {
     return out;
 }
 
-template <preety_printed<Data<int, extra_token>> T>
-struct std::formatter<T> : std::formatter<std::string> {
-    auto format(const T& t, [[maybe_unused]] std::format_context& ctx) const {
+template <>
+struct std::formatter<Data<int, extra_token>> : std::formatter<std::string> {
+    auto format(const Data<int, extra_token>& t, [[maybe_unused]] std::format_context& ctx) const {
         std::ostringstream o;
         o << t;
         return formatter<string>::format(std::move(o).str(), ctx);
     }
 };
 
-template <>
-struct UserDefaultValue<Damage, extra_token> {
-    static constexpr auto value = [] { return Damage{500}; };
+template <typename T>
+auto to_tuple(T&& type) {
+    using TUPLE = as_tuple<std::remove_cvref_t<T>>;
+    constexpr size_t count = std::tuple_size_v<TUPLE>;
+    return [&]<size_t... idx>(std::index_sequence<idx...>) {
+        return TUPLE{type.template getType<idx>()...};
+    }(std::make_index_sequence<count>{});
+};
+
+template <size_t N, typename T>
+decltype(auto) getWay(T&& type) {
+    if constexpr (requires { type.template getType<N>(); }) {
+        return std::forward<T>(type).template getType<N>();
+    } else {
+        return std::get<N>(std::forward<T>(type));
+    }
+}
+
+auto for_each_type = []<typename T, typename Fn>(T&& type, Fn fn) {
+    using TUPLE = as_tuple<std::remove_cvref_t<T>>;
+    constexpr size_t count = std::tuple_size_v<TUPLE>;
+
+    return [&]<size_t... idx>(std::index_sequence<idx...>) {
+        (..., fn(getWay<idx>(type)));
+    }(std::make_index_sequence<count>{});
 };
 
 int main() {
@@ -47,28 +76,34 @@ int main() {
         | With::user_property<int, extra_token>     //
         ;
 
-    std::vector entities{
-        create_entity(unordered,
-                      forTags<>(Health{100}),
-                      forTags<extra_token>(Health{150}),
-                      forTags<>(Damage{5}),  // TODO: mix with normal types
-                      forTags<extra_token>(Damage{150}),
-                      forTags<>(CureHealth{15}),
-                      forTags<extra_token>(25),
-                      forTags<>(Data<int, extra_token>{15}))};
+    auto entity = create_entity(unordered,
+                                forTags<>(Health{100}),
+                                forTags<extra_token>(Health{150}),
+                                forTags<>(Damage{5}),  // TODO: mix with normal types
+                                forTags<extra_token>(Damage{150}),
+                                forTags<>(CureHealth{15}),
+                                forTags<extra_token>(25),
+                                forTags<>(Data<int, extra_token>{15}));
 
-    std::println("{}", parse_type_name<decltype(create_entity)::result_type, Data<int, extra_token>>());
+    std::println("{}", name<decltype(entity)>());
 
-    for (auto entity : entities) {
-        [&]<size_t... idx>(std::index_sequence<idx...>) {
-            (..., std::println("{}", entity.template getType<idx>()));
-            std::cout << '\n';
-        }(std::make_index_sequence<7>{});
+    std::println("{}", parse_type_name<decltype(entity)>());
 
-        auto [e_dmg, e_hp, e_int] = std::tie(
-            entity.getTaged<0, extra_token>(),
-            entity.getTaged<1, extra_token>(),
-            entity.getTaged<2, extra_token>());
-        std::println("extra: {} {} ({})\n", e_dmg, e_hp, e_int);
-    }
+    for_each_type(entity,
+                  [](const auto& entry) { std::println("{}", entry); });
+
+    auto [e_dmg, e_hp, e_int] = std::tie(
+        entity.getTaged<0, extra_token>(),
+        entity.getTaged<1, extra_token>(),
+        entity.getTaged<2, extra_token>());
+    std::println("extra:\n\t{}\n\t{}\n\t({})\n", e_dmg, e_hp, e_int);
+
+    using entity_type = decltype(entity);
+    std::println("{}", name<as_tuple<entity_type>>());
+
+    auto tp = to_tuple(entity);
+    std::println();
+
+    for_each_type(tp,
+                  [](const auto& entry) { std::println("{}", entry); });
 }
