7d259c9e37e9a453d831411629d7fe15aca1e4d8
Runer <runer53248@gmail.com>
2025-11-04 17:04:54 +0100

    Updated UserProperty_ C-tors for unordered types. Updated marks.

--- Taumaturgia/Properties/Constructors.hpp
+++ Taumaturgia/Properties/Constructors.hpp
@@ -30,7 +30,7 @@ class Constructors<UserProperty_<TYPE, T, Tags...>> : public T {
 public:
     using hold_type = TYPE;
 
-    // MARK: Namingable tuple C-tors
+    // MARK: Namingable tuple
 
     template <typename... INFO, typename... Args>
         requires(not std::is_same_v<TYPE, Name>)
@@ -48,7 +48,7 @@ public:
         static_assert(constructible_from_args<TYPE, INFO...>, "Can't create TYPE from given tuple.");
     }
 
-    // MARK: Namingable default and ignore C-tors
+    // MARK: Namingable default and ignore
 
     constexpr Constructors(const Name& name)
         requires(not std::is_same_v<TYPE, Name> and std::constructible_from<T, Name>)
@@ -59,7 +59,7 @@ public:
         requires(not std::is_same_v<TYPE, Name>)
         : T{name, std::forward<Args>(args)...} {}
 
-    // MARK: Unordered C-tors
+    // MARK: Unordered Data<>
 
     template <typename TT, typename... TTags>
     constexpr Constructors(const Unordered&, Data<TT, TTags...>&& arg) {
@@ -76,30 +76,51 @@ public:
         getTypeTaged<TT, TTags...>(*static_cast<user_prop*>(this)) = arg.value;
     }
 
-    template <typename... Args>
-        requires std::same_as<boost::mp11::mp_unique<list<std::remove_cvref_t<Args>...>>, list<std::remove_cvref_t<Args>...>>  // every argument have unique type
-    constexpr Constructors(const Unordered&, Args&&... args)
-        : T{} {
-        // using user_prop = Constructors<UserProperty_<TYPE, T, Tags...>>;
+    // MARK: Unordered
+
+    template <typename Arg>
+    constexpr Constructors(const Unordered&, Arg&& arg) {
         using user_prop = UserProperty_<TYPE, T, Tags...>;
 
-        constexpr bool have_all_types_from_args = (trait_accessable<user_prop, std::remove_cvref_t<Args>> and ...);
-        if constexpr (have_all_types_from_args) {
-            ((trait<std::remove_cvref_t<Args>>::get(*static_cast<user_prop*>(this)) = std::forward<Args>(args)), ...);
-            // ((trait<std::remove_cvref_t<Args>>::get(*this) = std::forward<Args>(args)), ...);
+        if constexpr (trait_accessable<user_prop, std::remove_cvref_t<Arg>>) {
+            trait<std::remove_cvref_t<Arg>>::get(*static_cast<user_prop*>(this)) = std::forward<Arg>(arg);
         } else {
-            static_assert(have_all_types_from_args, "Unordered c-tor arguments contains type not accessable by traits.");
+            static_assert(false, "Unordered c-tor arguments contains type not accessable by traits.");
         }
     }
 
-    // MARK: copy/move C-tors
+    template <typename Arg, typename... Args>
+    constexpr Constructors(const Unordered& u, Arg&& arg, Args&&... args)
+        : Constructors{u, std::forward<Args>(args)...} {
+        using user_prop = UserProperty_<TYPE, T, Tags...>;
+
+        if constexpr (trait_accessable<user_prop, std::remove_cvref_t<Arg>>) {
+            trait<std::remove_cvref_t<Arg>>::get(*static_cast<user_prop*>(this)) = std::forward<Arg>(arg);
+        } else {
+            static_assert(false, "Unordered c-tor arguments contains type not accessable by traits.");
+        }
+    }
+
+    template <typename TT, typename... TTags, typename... Args>
+        requires(not std::same_as<
+                    boost::mp11::mp_unique<list<std::remove_cvref_t<Args>...>>,
+                    list<std::remove_cvref_t<Args>...>>)  // arguments are not of unique type
+    constexpr Constructors(const Unordered& u, Data<TT, TTags...>&& arg, Args&&... args) = delete;
+    
+    template <typename Arg, typename... Args>
+        requires(not std::same_as<
+                    boost::mp11::mp_unique<list<std::remove_cvref_t<Args>...>>,
+                    list<std::remove_cvref_t<Args>...>>)  // arguments are not of unique type
+    constexpr Constructors(const Unordered& u, Arg&& arg, Args&&... args) = delete;
+
+    // MARK: copy/move
 
     template <typename TT>
         requires(std::derived_from<T, std::remove_cvref_t<TT>>)
     constexpr explicit Constructors(TT&& t)
         : T{std::forward<TT>(t)} {}
 
-    // MARK: Namingable type C-tors
+    // MARK: Namingable type
 
     template <typename... Args>
     constexpr Constructors(const Name& name, TYPE&& type, Args&&... args)
@@ -111,7 +132,7 @@ public:
         requires(not std::is_same_v<TYPE, Name>)
         : T{name, std::forward<Args>(args)...}, type_{type} {}
 
-    // MARK: Namingable variant C-tors
+    // MARK: Namingable variant
 
     template <typename... V, typename... Args>
         requires contains_type<TYPE, V...> /*and Namingable<T>*/
@@ -126,7 +147,7 @@ public:
     constexpr Constructors(const Name& name, [[maybe_unused]] const std::variant<V...>& type, Args&&... args)
         : T{name, std::forward<Args>(args)...} {}
 
-    // MARK: tuple C-tors
+    // MARK: tuple
 
     template <typename... INFO, typename... Args>
     constexpr Constructors(std::tuple<INFO...>&& type, Args&&... args)
@@ -142,7 +163,7 @@ public:
         static_assert(constructible_from_args<TYPE, INFO...>, "Can't create TYPE from given tuple.");
     }
 
-    // MARK: default and ignore C-tors
+    // MARK: default and ignore
 
     constexpr Constructors() = default;
 
@@ -150,7 +171,7 @@ public:
     constexpr Constructors([[maybe_unused]] decltype(std::ignore) type, Args&&... args)
         : T{std::forward<Args>(args)...} {}
 
-    // MARK: type C-tors
+    // MARK: type
 
     template <typename... Args>
     constexpr Constructors(TYPE&& type, Args&&... args)
@@ -160,7 +181,7 @@ public:
     constexpr Constructors(const TYPE& type, Args&&... args)
         : T{std::forward<Args>(args)...}, type_{type} {}
 
-    // MARK: variant C-tors
+    // MARK: variant
 
     template <typename... V, typename... Args>
         requires contains_type<TYPE, V...>
