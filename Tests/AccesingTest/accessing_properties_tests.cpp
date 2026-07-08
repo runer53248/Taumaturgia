4f5f0da75de205cd69f0562b7d00d8ed38984d10
Runer <runer53248@gmail.com>
2026-07-08 04:53:11 +0200

    Fixed merge.

--- Tests/AccesingTest/accessing_properties_tests.cpp
+++ Tests/AccesingTest/accessing_properties_tests.cpp
@@ -16,49 +16,6 @@ struct tag_wrong {};
 
 static_assert(Property_ordered<Living>::value < Property_ordered<Damaging>::value);
 
-struct with_health {
-    // using hold_type = Health;
-    Health hp;
-};
-
-template <typename T>
-    requires std::is_base_of_v<with_health, T>
-struct HoldTypes<T> {
-    using types = list<Health>;
-};
-
-struct with_health_and_damage {
-private:
-    Health other_hp{};
-    Damage other_dmg{};
-
-public:
-    constexpr with_health_and_damage() = default;
-    constexpr with_health_and_damage(Health hp) : other_hp{hp} {}
-
-    template <size_t S, typename Self>
-    constexpr decltype(auto) getType(this Self& self) noexcept {
-        if constexpr (S == 0)
-            return (self.other_hp);
-        else
-            return (self.other_dmg);
-    }
-    template <size_t S, typename Self>
-        requires(S > 1)
-    constexpr decltype(auto) getType(this Self& self) noexcept = delete;
-
-    template <typename Type, isTag TAGS = no_tag, typename Self>
-        requires((std::same_as<Type, Health> or std::same_as<Type, Damage>) and TAGS::size == 0)
-    constexpr decltype(auto) getTypeTaged(this Self& self) noexcept {
-        if constexpr (std::same_as<Type, Health>)
-            return (self.other_hp);
-        if constexpr (std::same_as<Type, Damage>)
-            return (self.other_dmg);
-    }
-    template <typename Type, isTag TAGS, typename Self>
-    constexpr decltype(auto) getTypeTaged(this Self& self) noexcept = delete;
-};
-
 // MARK: properties
 
 template <typename T>
